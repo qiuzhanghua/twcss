2053018f277cfca7953ac0f478c11f75a4ea7d2d
qiuzhanghua <qiuzhanghua@icloud.com>
2022-05-14 21:36:16 +0800

    try replace some with tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	import './tailwind.css'
+	import './style.css'
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-	<h1 class="text-3xl font-bold bg-blue-400 w-1/2">
+<main class="m-0 p-1 text-center">
+	<h1 class="max-w-none text-red-700 text-6xl leading-tight font-thin bg-blue-400 w-1/2 lg:max-w-[32rem] mx-auto my-auto">
 		Hello TailwindCSS!
 	</h1>
   <Counter />
@@ -16,26 +16,25 @@
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+  /*main {*/
+  /*  text-align: center;*/
+  /*  padding: 1em;*/
+  /*  margin: 0 auto;*/
+  /*}*/
 
-  h1 {
-    color: #ff3e00;
+  /*h1 {*/
+    /*color: #ff3e00;*/
     /*text-transform: uppercase;*/
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
+    /*font-size: 4rem;*/
+    /*font-weight: 100;*/
+    /*line-height: 1.1;*/
+    /*margin: 2rem auto;*/
+    /*max-width: 14rem;*/
+  /*}*/
 
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-  }
+  /*@media (min-width: 480px) {*/
+  /*  h1 {*/
+  /*    max-width: none;*/
+  /*  }*/
+  /*}*/
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../tailwind.css';
+	import '../style.css';
 
   let count = 0
   const increment = () => {
@@ -7,7 +7,10 @@
   }
 </script>
 
-<button on:click={increment} class="bg-blue-500 accent-red-300 w-1/3">
+<button on:click={increment} class="bg-blue-500 accent-red-300
+				w-1/3 mt-4 px-4 py-4 rounded-[2em] border-2 tabular-nums
+				border-solid border-red-700 outline-none cursor-pointer
+				active:bg-yellow-500">
   Clicks: {count}
 </button>
 
@@ -15,22 +18,22 @@
   button {
     font-family: inherit;
     font-size: inherit;
-    padding: 1em 2em;
+    /*padding: 1em 2em;*/
     /*color: #ff3e00;*/
     /*background-color: rgba(255, 62, 0, 0.1);*/
-    border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
-    outline: none;
+    /*border-radius: 2em;*/
+    /*border: 2px solid rgba(255, 62, 0, 0);*/
+    /*outline: none;*/
     /*width: 200px;*/
-    font-variant-numeric: tabular-nums;
-    cursor: pointer;
+    /*font-variant-numeric: tabular-nums;*/
+    /*cursor: pointer;*/
   }
 
   button:focus {
     border: 2px solid #ff3e00;
   }
 
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
-  }
+  /*button:active {*/
+  /*  background-color: rgba(255, 62, 0, 0.2);*/
+  /*}*/
 </style>
